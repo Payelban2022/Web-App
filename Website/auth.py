5b5f3ead7a85b3d9f54746d6015539a7cf95df0f
Payel <payel.nandy2580@gmail.com>
2023-01-17 22:06:03 +0530

    Web app

--- Website/auth.py
+++ Website/auth.py
@@ -22,17 +22,16 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='ERROR')
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(firstName) or len(lastName) < 2:
-            flash('Name must be greater than 1 characters.', category='ERROR')
+            flash('Name must be greater than 1 characters.', category='error')
 
         elif password1 != password2:
-            flash('Passwords does not match.', category='ERROR')
+            flash('Passwords does not match.', category='error')
         elif len(password1) < 8:
-            flash(' Password must be at least 8 characters.', category='ERROR')
+            flash(' Password must be at least 8 characters.', category='error')
         else:
-
-            flash('Account created!', category='SUCCESS')
+            flash('Account created!', category='success')
 
 
 
